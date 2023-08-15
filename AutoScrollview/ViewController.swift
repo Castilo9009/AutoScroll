45d0f28479f4ca709fcafaebabb3b8733def25b5
Castilo Farmisi <141524234+Castilo9009@users.noreply.github.com>
2023-08-16 02:43:24 +0300

    view folder added

---
+++ AutoScrollview/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  AutoScrollview
+//
+//  Created by Zhou Hao on 26/08/15.
+//  Copyright © 2015年 Zeus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: WOWAutolayoutScrollViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
